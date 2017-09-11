bde20b0b7aac0c208ef08990eafbc8752cb1626c
Dorota Niezborała <d.niezborala@merixstudio.com>
2017-09-11 20:52:40 +0200

    Add difflib module, add german vocabulary
    Add difflib module to find the closest matches to german words and vocabulary

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*-
 import json
+from difflib import get_close_matches
 
 data = json.load(open("vocabulary.json"))
 
 def dictionary(w):
-    return data[w]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Czy chodziło Tobie o %s?" % get_close_matches(w, data.keys())[0]
+    else:
+        return "Słowo nie istnieje w słowniku. Spróbuj z innym słowem."
 
 word = input("Wpisz słowo: ")
 
