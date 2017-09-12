d420102257055a495bcc32bf47fd65dcab00bae9
Dorota Niezborała <d.niezborala@merixstudio.com>
2017-09-12 20:44:59 +0200

    Add more logic to the script
    work with conditionals and output

--- app.py
+++ app.py
@@ -9,10 +9,22 @@ def dictionary(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Czy chodziło Tobie o %s?" % get_close_matches(w, data.keys())[0]
+        yn = input("Czy chodziło Tobie o %s? Wpisz 't' jeśli tak lub wpisz 'n' jeśli nie: " % get_close_matches(w, data.keys())[0])
+        if yn == "t":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "n":
+            return "Słowo nie istnieje w słowniku. Spróbuj z innym słowem."
+        else:
+            return "Nie wiem czego szukasz. Może spóbujesz jeszcze raz?"
     else:
         return "Słowo nie istnieje w słowniku. Spróbuj z innym słowem."
 
 word = input("Wpisz słowo: ")
 
-print(dictionary(word))
+output = dictionary(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
